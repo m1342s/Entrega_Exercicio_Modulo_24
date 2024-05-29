9e79d4d87620aaace553040f90c36bb69a62ce3d
Matheus Viersa <matheusviersa995@hotmail.com>
2024-05-29 12:25:15 -0300

    Entrega atualizada

--- main.js
+++ main.js
@@ -6,23 +6,23 @@ this.anofabricacao=anofabricacao
 this.preco=preco
 }
 
-const revuelto= new Carro("revuelto","lamborghini","2025","2024",608.358)
-const veyron=new Carro("veyron","bugatti","2006","2005",1700000)
-const continental= new Carro("continental","bentley","2013","2012",219.930)
 
-function Luxo(tipo,pais,blindagem){
+
+function Luxo(tipo,pais,blindagem,modelo){
     this.tipo=tipo
     this.pais=pais
     this.blindagem=blindagem
     Carro.call(this,modelo)
-    Carro.call(this,fabricante)
-    Carro.call(this,preco)
+   
 }
 
-function Popular(marca,motor,pneus) {
+function Popular(marca,motor,pneus,preco) {
     this.marca=marca
     this.motor=motor
     this.pneus=pneus
-    Carro.call(this,modelo)
     Carro.call(this,preco)
 }
+
+const revuelto= new Luxo("coupe","italia","nao","revuelto","lamborghini")
+const kwid=new Popular("Renault",1.0,14,"kwid",73600)
+const continental= new Luxo("coupe","inglaterra","sim","continental")
